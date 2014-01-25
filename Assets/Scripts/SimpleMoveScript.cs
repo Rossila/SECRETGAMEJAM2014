6b9ad1902b0fc78950ea0ccc034767acd8f5a5ba
Garwood Pang <garwoodpang@gmail.com>
2014-01-24 21:35:25 -0800

    Initial Commit for secert gamejam project

---
+++ Assets/Scripts/SimpleMoveScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class SimpleMoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed;
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction;
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
