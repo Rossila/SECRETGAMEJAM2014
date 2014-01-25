7d02366165636a2fa9d70960561cd8bbc1cc9a25
Geran Tam <geran_tam@hotmail.com>
2014-01-25 02:45:08 -0800

    Created new physics material for the ground (no friction), created prefab for rabbit, scripted motion, jumping, and collision detection for ground. Implemented placeholder ground sprites with tags for ground to test collision detection. ARE YOU HAPPY GARWOOD?

---
+++ Assets/Scripts/RabbitScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class RabbitScript : MonoBehaviour {
+	public static float distanceTraveled;
+
+	public Vector3 startScale;
+
+	public Vector2 jumpSpeed;
+	public Vector2 constantSpeed;
+	
+	public float gameOverY;
+	public float startMass;
+
+	private bool onGround;
+	private Vector2 startPosition;
+
+	void Start () 
+	{
+		startPosition = transform.localPosition;
+		gameStart ();
+	}
+
+	void Update()
+	{
+		if(Input.GetButton("Jump") && onGround){
+			rigidbody2D.AddForce(jumpSpeed);
+			Debug.Log(rigidbody2D.velocity.y);
+			onGround = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = new Vector2 (constantSpeed.x / rigidbody2D.mass, rigidbody2D.velocity.y);
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		if ((other.gameObject.tag == "ground")) {
+			onGround = true;
+		} 
+
+	}
+
+	void gameStart()
+	{
+		transform.localPosition = startPosition;
+		rigidbody2D.mass = startMass;
+		transform.localScale = startScale;
+	}
+
+	void gameOver()
+	{
+		rigidbody2D.velocity = Vector2.zero;
+	}
+}
