4710a6e791e0be2b25d643f655c0199a6a7f3e7e
Amanda Tu <tu.amanda3@gmail.com>
2014-01-25 14:38:22 -0800

    Modified Carrot script

--- Assets/Scripts/CakeScript.cs
+++ Assets/Scripts/CakeScript.cs
@@ -2,14 +2,58 @@
 using System.Collections;
 
 public class CakeScript : MonoBehaviour {
+	private bool hasSpawn;
 
 	// Use this for initialization
 	void Start () {
-	
+		//startPosition = transform.localPosition;
+		//gameStart ();
+		hasSpawn = false;
+		
+		// Disable everything
+		// -- collider
+		collider2D.enabled = false;
+	}
+
+	bool hitWall()
+	{
+		Vector2 currentPosition = new Vector2 (transform.position.x, transform.position.y);
+		return Physics2D.Raycast(currentPosition + Vector2.right*0.73f, Vector2.right, 0.1f);
 	}
 	
+	void OnCollisionEnter2D(Collision2D other){
+		if ((other.gameObject.tag == "rabbit")) {// && hitWall ()) {
+			Destroy (gameObject);
+		} 
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false)
+		{
+			if (renderer.IsVisibleFrom(Camera.main))
+			{
+				Spawn();
+			}
+		}
+		else
+		{
+			
+			// 4 - Out of the camera ? Destroy the game object.
+			if (renderer.IsVisibleFrom(Camera.main) == false)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void Spawn()
+	{
+		hasSpawn = true;
+		
+		// Enable everything
+		// -- Collider
+		collider2D.enabled = true;
 	}
 }
