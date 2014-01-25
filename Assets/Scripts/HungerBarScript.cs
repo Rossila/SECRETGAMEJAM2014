91f7f4d896d5bd16eda78a59fe49bd537f289226
Flora Fong <flora.lt.fong@gmail.com>
2014-01-25 13:11:03 -0800

    Added stuff for the energy bar

---
+++ Assets/Scripts/HungerBarScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HungerBarScript : MonoBehaviour {
+	
+	public float maxLength = 600;
+	private float length;
+
+	// energy
+	private float maxEnergy = 100f;
+	private float currEnergy;
+	private float hungerSpeed = -1f;
+
+	// slider offset to fit the energy bar
+	public Vector2 offset = new Vector2(40, 40);
+
+	// make slider white for tinting
+	public GUIStyle energySlider;
+	private int sliderHeight = 40;
+	private int sliderWidth = 5;
+
+	// Use this for initialization
+	void Start () {
+		currEnergy = maxEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AdjustCurrEnergy (hungerSpeed);
+	}
+
+	void OnGUI() {
+		GUI.BeginGroup (new Rect (offset.x, offset.y, maxLength, sliderHeight));
+		GUI.backgroundColor = Color.red;
+
+		// energy slider
+		GUI.Box(new Rect(length, 0, 10, sliderHeight), "", energySlider);
+
+		GUI.EndGroup();
+	}
+	
+	public void AdjustCurrEnergy(float i) {
+		currEnergy += i * 0.1f;
+		
+		if (currEnergy < 0) {
+			currEnergy = 0;
+		} else if (currEnergy > maxEnergy) {
+			currEnergy = maxEnergy;
+		}
+
+		length = (currEnergy / maxEnergy) * maxLength;
+	}
+
+}
