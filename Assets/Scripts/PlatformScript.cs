b50032dbc0371271040691bc30c087849a750c97
Garwood Pang <garwoodpang@gmail.com>
2014-01-25 16:24:22 -0800

    Updated Platform prefabs so platform is created and destory depending on the camera

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : MonoBehaviour {
+	private bool hasSpawn;
+	
+	// Use this for initialization
+	void Start () {
+		//startPosition = transform.localPosition;
+		//gameStart ();
+		hasSpawn = false;
+		
+		// Disable everything
+		// -- collider
+		collider2D.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 2 - Check if the enemy has spawned.
+		if (hasSpawn == false)
+		{
+			if (renderer.IsVisibleFrom(Camera.main))
+			{
+				Spawn();
+			}
+		}
+		else
+		{
+			// 4 - Out of the camera ? Destroy the game object.
+			if (renderer.IsVisibleFrom(Camera.main) == false)
+			{
+				gameObject.SetActive (false);
+			}
+		}
+	}
+	
+	private void Spawn()
+	{
+		hasSpawn = true;
+		
+		// Enable everything
+		// -- Collider
+		collider2D.enabled = true;
+	}
+}
