59bc14dde43d43dabd703995072f5b6f9ad2c67a
Julien Law <julienlaw1@gmail.com>
2014-01-26 03:15:21 -0800

    added a victory screen; similar to game over screen

---
+++ Assets/Scripts/VictoryScreenScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryScreenScript : MonoBehaviour {
+
+	public GUIStyle skin;
+	
+	private int width = 400;
+	private int height = 400;
+	private Vector2 offset;
+	
+	public bool isGameWin = false;
+
+	// Use this for initialization
+	void Start () {
+
+		this.offset = new Vector2 (Screen.width / 2, Screen.height / 2);
+		GameManager.GameWin += gameWin;
+		GameManager.GameStart += gameStart;
+		GameManager.GameOver += gameOver;
+		gameStart ();
+	}
+
+	void gameOver () {
+
+	}
+
+	void gameStart () {
+		gameObject.SetActive (false);
+	}
+
+	void gameWin() {
+		gameObject.SetActive (true);
+		Time.timeScale = 0;
+	}
+	
+	void OnGUI() {
+		GUIStyle h1 = new GUIStyle();
+		h1.fontSize = 32;
+		h1.normal.textColor = Color.white;
+		
+		GUI.BeginGroup (new Rect (offset.x, offset.y - 10, width, height));
+		
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label("YOU WON", h1);
+		GUILayout.EndHorizontal ();
+		
+		GUILayout.BeginHorizontal ();
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUILayout.Button("Try Again?")) {
+			Application.LoadLevel("Level 1");
+		}
+		
+		// Make the second button.
+		if(GUILayout.Button("Main Menu")) {
+			Application.LoadLevel("MainMenu");
+		}
+		
+		GUILayout.EndHorizontal ();
+		GUI.EndGroup ();
+	}
+
+	void OnDestroy() {
+		GameManager.GameWin -= gameWin;
+		GameManager.GameStart -= gameStart;
+		GameManager.GameOver -= gameOver;
+	}
+}
